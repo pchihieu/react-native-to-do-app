eb8727a2f1ea20cc89bbc2e9a39a89218f102d19
Phan Chí Hiếu <phanchihieu1701@gmail.com>
2024-07-02 15:03:13 +0700

    double check on updating

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -6,7 +6,7 @@ import { ExternalLink } from '@/components/ExternalLink';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-
+// this is for double check
 export default function TabTwoScreen() {
   return (
     <ParallaxScrollView
